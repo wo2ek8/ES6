db9c690791db513c6389efb1705ecf18740590f6
손재이 <jae2son@gmail.com>
2020-06-11 03:46:51 +0900

    this

--- app.js
+++ app.js
@@ -19,8 +19,41 @@ const hearts = names.map(addHeart);*/
     return item + '💖';
 });*/
 
-/*const hearts = names.map((item, index) => item + ' 💖 ' + index);*/
+const hearts = names.map((item, index) => item + ' 💖 ' + index);
 
-const hearts = names.map(() => '💖');
+//const hearts = names.map(() => '💖');
 
 console.log(hearts);
+
+const button = document.querySelector('button');
+
+/*button.addEventListener('click', function() {
+    console.log(this);
+    console.log('i have been clicked');
+    
+    this.style.backgroundColor = 'red';
+});*/
+
+/*button.addEventListener('click', () => {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+});*/
+
+/*const handleClick = () => {
+    console.log(this);
+};
+
+button.addEventListener('click', handleClick);*/
+
+const jaei = {
+    name: 'Jaei',
+    age: 23,
+    addYear() {
+        this.age++;
+    }
+};
+
+console.log(jaei);
+jaei.addYear();
+jaei.addYear();
+console.log(jaei);
