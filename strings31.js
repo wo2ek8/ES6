794d45fb04852c1f4cd64a0ba58c6c9690f44366
손재이 <jae2son@gmail.com>
2020-06-18 00:29:28 +0900

    HTML Fragments

---
+++ strings31.js
@@ -0,0 +1,32 @@
+/*const wrapper = document.querySelector('.wrapper');*/
+
+/*const addWelcome = () => {
+    const div = document.createElement('div');
+    const potato = document.createElement('div');
+    const h1 = document.createElement('h1');
+    h1.innerHTML = 'Hello';
+    h1.className = 'sexyTitle';
+    div.append(h1);
+    wrapper.append(div);
+    potato.append(h1);
+    const div = `
+        <div class="hello">
+            <h1 class="title">Hello</h1>
+        </div>
+    `;
+    const div = '<div class="hello"><h1 class="title">Hello</h1></div>';
+    wrapper.append(potato);
+    wrapper.innerHTML = div;
+}*/
+
+/*setTimeout(addWelcome, 5000);*/
+/*setTimeout(addWelcome, 2000);*/
+
+console.log(`
+
+
+
+
+    hello
+
+`);
