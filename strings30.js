aba8d8e6ed91646f94dac780ddd72fc6febdfd39
손재이 <jae2son@gmail.com>
2020-06-17 14:25:12 +0900

    sexy strings

---
+++ strings30.js
@@ -0,0 +1,13 @@
+/*const sayHi = (aName = 'jihan') => 'hello ' + aName + ' lovely to have you';*/
+
+/*const sayHi = (aName = 'jihan') => `hello ${aName} lovely to have you`;*/
+
+/*console.log(sayHi());*/
+
+/*console.log(`hello how are you ${1000 * 39383}`);*/
+
+const add = (a, b) => a + b;
+
+/*console.log(`hello how are you ${add(6, 6)}`);*/
+
+console.log(`hello how are you ${add}`);
