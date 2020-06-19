246b4140dd73b29d8b501014eff122c8f26048de
손재이 <jae2son@gmail.com>
2020-06-19 22:22:02 +0900

    HTML Fragments part Two

---
+++ strings32.js
@@ -0,0 +1,37 @@
+const wrapper = document.querySelector('.wrapper');
+
+const friends = ['me', 'woohyub', 'jihan', 'minbyeong'];
+
+const ul = document.createElement('ul');
+/*friends.forEach(friend => ul.append(`<li>${friend}</li>`));*/
+
+/*const list = `
+    <h1>People i love</h1>
+        <ul>
+            ${friends.map(friend => `<li>${friend}</li>`).join('💖')}
+        </ul>
+`;*/
+
+/*const list = `
+    <h1>People i love</h1>
+        <ul>
+            ${friends.map(friend => `<li>${friend}</li>`).join(' ')}
+        </ul>
+`;*/
+
+/*const list = `
+    <h1>People i love</h1>
+        <ul>
+            ${friends.map(friend => `<li>${friend}</li>`).join('')}
+        </ul>
+`;*/
+
+const list = `
+    <h1>People i love</h1>
+        <ul>
+            ${friends.map(friend => `<li>${friend}💖</li>`).join('')}
+        </ul>
+`;
+
+/*wrapper.append(ul);*/
+wrapper.innerHTML = list;
