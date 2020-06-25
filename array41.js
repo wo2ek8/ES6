eaf7776a9fcabf6a02186d8893bc3ff1c52eec36
손재이 <jae2son@gmail.com>
2020-06-25 12:06:28 +0900

    Array.fill()

--- array41.js
+++ array41.js
@@ -13,20 +13,54 @@ const friends = [
     'minbyeong@gorea.com'
 ];
 
+/*const check = () => friends.findIndex(friend => friend.includes('@gorea.com'));*/
+
 /*const target = friends.find(friend => friend.includes('@korea.com'));*/
 /*const target = friends.find(friend => true);*/
-const target = friends.findIndex(friend => friend.includes('@gorea.com'));
+/*const target = friends.findIndex(friend => friend.includes('@gorea.com'));*/
+/*let target = friends.findIndex(friend => friend.includes('@gorea.com'));*/
+/*let target = check();*/
 
-console.log(target);
+/*console.log(target);*/
 
 /*friends[target] = 'minbyeong@korea.com';*/
 
-console.log(friends);
+/*console.log(friends);*/
 
-const username = friends[target].split('@')[0];
+/*const username = friends[target].split('@')[0];*/
 
-const email = 'korea.com';
+/*const email = 'korea.com';*/
 
-console.log(username, email);
+/*console.log(username, email);*/
 
-console.log(`${username}@${email}`);
+/*console.log(`${username}@${email}`);*/
+/*friends[target] = `${username}@${email}`;*/
+
+/*target = friends.findIndex(friend => friend.includes('@gorea.com'));*/
+
+/*target = check();*/
+
+/*if(target !== -1) {
+    console.log(target);
+    
+    const username = friends[target].split('@')[0];
+    
+    const email = 'korea.com';
+    
+    friends[target] = `${username}@${email}`;
+    
+    target = check();
+}*/
+/*if(target !== -1) {
+    friends.fill('*'.repeat(5), target);
+}*/
+
+/*console.log(target);*/
+
+/*friends.fill('*'.repeat('5'), 1);*/
+/*friends.fill('*'.repeat('5'), 1, 3);*/
+/*friends.fill('*'.repeat('5'));*/
+
+/*console.log(friends);*/
+
+console.log(friends.includes('jaei@gmail.com'));
