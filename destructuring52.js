763cc7ca1a16e5c6e4a27fba4a7d56868a76344c
손재이 <jae2son@gmail.com>
2020-07-02 20:24:04 +0900

    Renaming

--- destructuring52.js
+++ destructuring52.js
@@ -4,8 +4,35 @@ const settings = {
     }
 };
 
-const {
+/*const {
     color: {chosen_color = 'light'}
-} = settings;
+} = settings;*/
 
-console.log(chosen_color);
+/*const {
+    color: {chosen_color: chosenColor = 'light'}
+} = settings;*/
+
+/*let {
+    color: {chosen_color: chosenColor = 'light'}
+} = settings;*/
+
+let chosenColor = 'blue';
+
+console.log(chosenColor);
+
+/*const {
+    color: {chosen_color: chosenColor = 'light'}
+} = settings;*/
+
+/*let {
+    color: {chosen_color: chosenColor = 'light'}
+} = settings;*/
+
+({
+    color: {chosen_color: chosenColor = 'light'}
+} = settings);
+
+/*const chosenColor = settings.color.chosen_color || 'light';*/
+
+/*console.log(chosen_color);*/
+console.log(chosenColor);
