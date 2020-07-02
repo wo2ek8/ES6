39c4b85d64051432fbc98805c6665eb4b51c4101
손재이 <jae2son@gmail.com>
2020-07-02 20:46:49 +0900

    Value Shorthands

---
+++ destructuring54.js
@@ -0,0 +1,16 @@
+const follow = checkFollow();
+const alert = checkAlert();
+
+/*const settings = {
+    notifications: {
+        follow: follow,
+        alert: alert
+    }
+};*/
+
+const settings = {
+    notifications: {
+        follow,
+        alert
+    }
+};
