0dd31288ed6f706b9c987fbb6dc42e9d1d3979cd
손재이 <jae2son@gmail.com>
2020-07-05 13:30:05 +0900

    Spread Applications

--- restAndSpread61.js
+++ restAndSpread61.js
@@ -5,8 +5,44 @@
 
 console.log(newFriends);*/
 
-const jaei = {
+/*const jaei = {
     username: 'jaei'
 };
 
-console.log({...jaei, password: 123});
+console.log({...jaei, password: 123});*/
+
+/*const first = ['mon', 'tue', 'wed'];
+
+const weekend = ['sat', 'sun'];
+
+const fullWeek = [...first, 'thu', 'fri', ...weekend];
+
+console.log(fullWeek);*/
+
+const lastName = prompt('Last name');
+
+/*const user = {
+    username: 'jaei',
+    age: 23
+};*/
+
+/*const user = {
+    username: 'jaei',
+    age: 23,
+    lastName: lastName !== '' ? lastName : undefined
+};*/
+
+/*const user = {
+    username: 'jaei',
+    age: 23,
+    ...(lastName !== '' && lastName)
+};*/
+
+const user = {
+    username: 'jaei',
+    age: 23,
+    ...(lastName !== '' && {lastName})
+};
+
+/*console.log(typeof lastName);*/
+console.log(user);
