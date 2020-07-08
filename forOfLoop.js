b16c837c71359d90fc4b4e7c8e2901fc501c18c3
손재이 <jae2son@gmail.com>
2020-07-08 10:37:53 +0900

    For ... of

---
+++ forOfLoop.js
@@ -0,0 +1,33 @@
+const friends = ['Jaei', 'Jihan', 'Yuchan', 'Seungjae', 'Woohyub', 'Judith', 'Nicholas'];
+
+/*for(let i  = 0; i < 20; i++) {
+    console.log('what');
+}*/
+/*for(let i  = 0; i < friends.length; i++) {
+    console.log(`${i}🙋‍♀️`);
+    console.log(`${friends[i]}🙋‍♀️`);
+}*/
+
+/*const jaeida = (c, i) => console.log(i, c + '🙋‍♀️')*/
+
+/*const jaeida = (currentItem, index, currentArray) => console.log(c, i, a);
+
+friends.forEach(jaeida);*/
+
+/*friends.forEach(friend => console.log(friend));*/
+
+/*for(const friend of friends) {
+    console.log(friend);
+}*/
+
+/*for(const letter of 'Hello this is very loooooooooong') {
+    console.log(letter);
+}*/
+
+for(const friend of friends) {
+    if(friend === 'Woohyub') {
+        break;
+    } else {
+        console.log(friend);
+    }
+}
